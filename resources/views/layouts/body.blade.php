f547dec2aab82f9d13e89d75589f0082e1766faa
destroylord <masapin68@gmail.com>
2020-04-18 23:29:13 +0700

    ubah design form login dan register

--- resources/views/layouts/body.blade.php
+++ resources/views/layouts/body.blade.php
@@ -3,11 +3,21 @@
 @section('head')
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+
+	<!-- Styles -->
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+	<style>
+		*{
+			font-family: 'Poppins', sans-serif;
+		}
+	</style>
+
+	<link rel="stylesheet" type="text/css" href="/assets_login/css/util.css">
+	<link rel="stylesheet" type="text/css" href="/assets_login/css/main.css">
+	
+
 @endsection
 
 @section('body')
