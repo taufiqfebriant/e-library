78f50aee87d7b519f993b21b05d897447f5cdb54
Muhammad Taufiq Febrianto <taufiqisfebrianto06@gmail.com>
2020-04-19 16:08:25 +0700

    Tabel subscriptions, plans, dan transactions

---
+++ database/migrations/2020_04_19_073552_create_transactions_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('restrict')
+                ->onDelete('cascade');
+            $table->foreignId('plan_id')
+                ->constrained()
+                ->onUpdate('restrict')
+                ->onDelete('cascade');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('paid_at')->nullable();
+            $table->string('receipt');
+            $table->unsignedBigInteger('confirmed_by');
+            $table->timestamp('confirmed_at')->nullable();
+
+            $table->foreign('confirmed_by')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('restrict')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
