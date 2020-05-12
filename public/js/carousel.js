f32b2af5c6d3b48f56ffbbbfba063d264733c433
ibnub <ibnubatutah001@gmail.com>
2020-05-12 14:20:30 +0700

    slick

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,5 +1,35 @@
 $(function () {
     $('.book-carousel').slick({
         slidesToShow: 6,
-    })
+        reponsive: [
+            {
+                breakpoint: 1200,
+                setting: {
+                    slidesToShow: 5,
+                    slidesToScroll: 5,
+                }
+            },
+            {
+                breakpoint: 1008,
+                setting: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 800,
+                setting: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                setting: 'unslick'
+            }
+        ]
+    });
+
+    $(window).on('resize', function() {
+        $('.book-carousel').slick('resize');
+    });
 })
