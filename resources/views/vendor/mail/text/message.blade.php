67a39f9e5264cf59d32812adf04c765e3ffbad6f
Muhammad Taufiq Febrianto <taufiqisfebrianto06@gmail.com>
2020-05-22 15:57:01 +0700

    Verifikasi email dengan SMTP Gmail

---
+++ resources/views/vendor/mail/text/message.blade.php
@@ -0,0 +1,27 @@
+@component('mail::layout')
+    {{-- Header --}}
+    @slot('header')
+        @component('mail::header', ['url' => config('app.url')])
+            {{ Str::of(config('app.name'))->replace('_', ' ') }}
+        @endcomponent
+    @endslot
+
+    {{-- Body --}}
+    {{ $slot }}
+
+    {{-- Subcopy --}}
+    @isset($subcopy)
+        @slot('subcopy')
+            @component('mail::subcopy')
+                {{ $subcopy }}
+            @endcomponent
+        @endslot
+    @endisset
+
+    {{-- Footer --}}
+    @slot('footer')
+        @component('mail::footer')
+            © {{ date('Y') }} {{ Str::of(config('app.name'))->replace('_', ' ') }}. @lang('All rights reserved.')
+        @endcomponent
+    @endslot
+@endcomponent
