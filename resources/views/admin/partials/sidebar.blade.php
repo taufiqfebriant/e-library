680e848e617a8ac7f87f2b5b04ac389e4ee7f9ac
Muhammad Taufiq Febrianto <taufiqisfebrianto06@gmail.com>
2020-05-22 13:14:24 +0700

    Nama aplikasi di sidebar admin

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -2,7 +2,7 @@
     <!-- Brand Logo -->
     <a href="{{ route('home.index') }}" class="brand-link">
         <i class="fas fa-book ml-3 mr-2"></i>
-        <span class="brand-text font-weight-light">{{ Str::of(config('app.name', 'Rumah_Cahaya_FLP_Saudi_Arabia'))->replace('_', ' ') }}</span>
+        <span class="brand-text font-weight-light">{{ Str::of(config('app.name', 'Rumah_Cahaya_FLP_Saudi_Arabia'))->replace('_', ' ')->replace(' FLP Saudi Arabia', '') }}</span>
     </a>
     <!-- Sidebar -->
     <div class="sidebar">
